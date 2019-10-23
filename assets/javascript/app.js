cb17942472172a4dcfd4f5fdc2f42860b8c95633
Lance Meara <meara.ty@gmail.com>
2019-10-22 21:17:35 -0500

    more logic, added ids

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,7 +4,6 @@
 //call to alpha vantage to get the stock ticker
 function stockTickerRequest(search) {
     //FOR TESTING PURPOSES
-    search = "google";
 
     var apiKey = "TFCCMLBLD91O6OFT";
     var queryURL = "https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords=" + search + "&apikey=" + apiKey;
@@ -18,7 +17,10 @@ function stockTickerRequest(search) {
         console.log("---------------------------------------------------------------");
 
         //grabbing the symbol from the first response of the search query
-        var responseTicker = response.bestMatches[0]["1. symbol"]
+        var responseTicker = response.bestMatches[0]["1. symbol"];
+
+        //display company name on html
+        $("#stock-name").text(response.bestMatches[0]["1. symbol"] + " - " + response.bestMatches[0]["2. name"]);
 
         stockDataRequest(responseTicker);
         newsRequest(responseTicker);
@@ -40,27 +42,28 @@ function stockDataRequest(ticker) {
 
         //changing the objects of objects into an array of objects
         var responseArray = [];
-        for(item in response["Monthly Adjusted Time Series"]){
+        for (item in response["Monthly Adjusted Time Series"]) {
             responseArray.push(response["Monthly Adjusted Time Series"][item]);
-         }
+        }
 
         console.log("response array")
         console.log(responseArray);
         console.log("---------------------------------------------------------------");
 
         //storing 4 values in an array to show quarterly growth
-        var dataArray = [];
+        var quarterlyData = [];
         for (var i = 13; i > 0; i -= 4) {
-            dataArray.push(responseArray[i]["5. adjusted close"]);
+            quarterlyData.push(responseArray[i]["5. adjusted close"]);
         }
 
-        worthBuy(dataArray);
+        worthBuy(quarterlyData);
         console.log("Above this is the data array");
-        console.log(dataArray);
+        console.log(quarterlyData);
         console.log("---------------------------------------------------------------");
 
         //passing through the data we retrieved to the displayChart function to show the chart on screen
-        displayChart(dataArray);
+        displayChart(quarterlyData);
+        displayStats(responseArray[0]);
 
 
     });
@@ -70,7 +73,6 @@ function stockDataRequest(ticker) {
 function newsRequest(tickerSearch) {
     var apiKey = "44ec6ee2a9c74c3dbe590b43546a857c";
     var queryURL = "https://newsapi.org/v2/everything?q=" + tickerSearch + "&apiKey=" + apiKey;
-    console.log(queryURL);
 
     $.ajax({
         url: queryURL,
@@ -100,19 +102,46 @@ function newsRequest(tickerSearch) {
 //append article cards to display
 function displayArticles(articles) {
     var title = "";
-    var description = "";
     var publishedDate = "";
     var source = "";
     var newsUrl = "";
+    var newsImage = "";
+    //empty current article display
+    $("#article-display").empty();
 
     for (var i = 0; articles.length; i++) {
         title = articles[i].title;
-        description = articles[i].description;
         publishedDate = articles[i].publishedAt.substring(0, 10);
         source = articles[i].source.name;
         newsUrl = articles[i].url;
+        newsImage = articles[i].urlToImage;
+
+        $("#article-display").append("<div class='card'>" +
+            "<img class='card-img-top' src=" + newsImage + " alt=" + title + " width='385' height='214'>" +
+            "<div class='card-body'>" +
+            "<h5 class='card-title'>" +
+            "<a href='" + newsUrl + "' target='_blank' class='card-link'>" + title + "</a>" +
+            "</h5>" +
+            "<p class='card-text'>" + source + "</p>" +
+            "<p>" + publishedDate + "</p>" +
+            "</div>" +
+            "</div>");
 
     }
+}
+
+function displayStats(current) {
+    var open = "";
+    var high = "";
+    var low = "";
+
+    open = current["1. open"];
+    high = current["2. high"];
+    low = current["3. low"];
+
+    $("#open-close").text("$" + open.substring(0, open.length - 2));
+    $("#todays-high").text("$" + high.substring(0, open.length - 2));
+    $("#todays-low").text("$" + low.substring(0, open.length - 2));
 }
 
 function displayChart(stockData) {
@@ -145,29 +174,43 @@ function displayChart(stockData) {
 function worthBuy(yearlyData) {
     var percent = 0;
 
-    percent = ((yearlyData[3]-yearlyData[0])/yearlyData[0])*100;
+    percent = ((yearlyData[3] - yearlyData[0]) / yearlyData[0]) * 100;
     console.log("percent");
     console.log(percent);
     console.log("---------------------------------------------------------------");
-}
 
+    //check if the stock is worth buying or not
+    if (percent >= 3) {
+        $("#buy-sell").text("Buy");
+    }
+    else {
+        $("#buy-sell").text("Don't Buy");
+    }
+}
 
 
 //EVENT HANDLERS
 ////////////////////////////////////////////////////////////////////
-$("#search-button").on("click", function () {
-    stockTickerRequest($("#search-bar").text().trim());
-});
+$(document).ready(function () {
+    $("#search-button").on("click", function () {
+        console.log("clickerd");
+        event.preventDefault();
+        console.log($("#search-bar").val().trim());
+        if ($("#search-bar").length > 0) {
+            stockTickerRequest($("#search-bar").val().trim());
+        }
 
-$("bigSearchButton").on("click", function() {
+    });
+});
+$("#bigSearchButton").on("click", function () {
     //make this get the value of the main page and load next page with stored value
     // localStorage.setItem("defaultSearch", )
 });
 
 
-if(localStorage.getItem("defaultSearch") !== null)
-{
+if (localStorage.getItem("defaultSearch") !== null) {
     stockTickerRequest(localStorage.getItem("defaultSearch"));
 }
 //FOR TESTING PURPOSES
-stockTickerRequest();
+stockTickerRequest("intel");
+
